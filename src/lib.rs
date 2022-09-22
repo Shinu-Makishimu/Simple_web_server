c2893132b5296520eb955d873a7ac3376e0b2e29
Максим Семенов <maxim-semjonov@yandex.ru>
2022-09-22 19:39:39 +0200

    add lib.rc add pool

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use std::{
+    sync::{mpsc, Arc, Mutex}
+    thread,
+};
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+struct Job;
+
+
+impl ThreadPool {
+    /// Create new ThreadPool
+    ///
+    /// The size is the number of threads in the pool
+    ///
+    /// # Panic
+    ///
+    /// The 'new' func will panic if size is zero
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        let(sender, receiver) = mpsc::channel();
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where F: FnOnce() + Send + 'static,
+    {
+
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id:usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>s) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver
+        });
+        Worker {id, thread}
+    }
+}
